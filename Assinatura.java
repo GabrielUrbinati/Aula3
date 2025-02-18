da66191069dc79cc0e48aa33c90747ea73100b3f
GabrielUrbinati <152566779+GabrielUrbinati@users.noreply.github.com>
2025-02-17 22:27:45 -0300

    Update Assinatura.java

--- Assinatura.java
+++ Assinatura.java
@@ -1,11 +1,17 @@
 public class Assinatura extends Jogo {
-    public Assinatura(Sting nome , String tipo, double valor){
+    private String plano;
+    public Assinatura(Sting nome , String tipo, double valor, String plano){
             super(nome, tipo, valor);
+            this.plano=plano;
         }
         @Override
         public void cobrar(){
             System.out.println("Enviando cobrança automatica mensal");
-            System.out.println("dados do jogo adquirido");
+            System.out.println("dados do produto adquirido");
             System.out.println(super.toString());
+            System.out.println("Plano " + plano);
+        }
+        public String getPlano(){
+            return plano;
         }
 }
