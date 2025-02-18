618ef48c072b59dd9b5505a723c6b25b8e07a1af
GabrielUrbinati <152566779+GabrielUrbinati@users.noreply.github.com>
2025-02-17 22:20:19 -0300

    Create App.java
    classe

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App{
+    publis static void main(Strng args[]) throws Exception{
+        Jogo j1 = new OneTimeBuy("Dead Space","Terror",89,00);
+        Jogo j2= new Assinatura ("GamePass","Todos",120,00);
+        j1.cobrar();
+        j2.cobrar();
+    }
+}
