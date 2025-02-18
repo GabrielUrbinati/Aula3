e2efe3d06ae092b31cfc2384e8d3d4b47093a7cf
GabrielUrbinati <152566779+GabrielUrbinati@users.noreply.github.com>
2025-02-17 22:27:16 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App{
     publis static void main(Strng args[]) throws Exception{
         Jogo j1 = new OneTimeBuy("Dead Space","Terror",89,00);
-        Jogo j2= new Assinatura ("GamePass","Todos",120,00);
+        Jogo j2= new Assinatura ("PS Plus","Todos",120,00,"Gold");
         j1.cobrar();
         j2.cobrar();
     }
